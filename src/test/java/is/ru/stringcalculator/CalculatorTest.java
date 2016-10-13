f77468626e8c50a096a01dbcd7e72716fe48dee1
brietk <brietkonn@gmail.com>
2016-10-13 00:02:16 +0000

    New line between numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -25,4 +25,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    @Test
+    public void testNewLine(){
+    	assertEquals(6, Calculator.add("1\n2\n3"));
+    }
+
 }
